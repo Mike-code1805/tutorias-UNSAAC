204ddf844d4bb5c93a3f5b069923d0f95fed301f
Miguel <miguelenrique1805@gmail.com>
2022-10-22 23:39:47 -0500

    refactor css

--- index.php
+++ index.php
@@ -7,27 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <title>Tutorias UNSAAC</title>
-    <style>
-    .button {
-      border: none;
-      color: white;
-      padding: 10px 32px;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      cursor: pointer;
-      background-color: #008cba;
-      border-radius: 10px;
-      box-shadow: -5px 5px 5px #98afa9;
-      width: auto;
-    }
-    .button:hover {
-      background-color: #00ba8b;
-    }
-    .button:active {
-      background-color: #252d97;
-    }
-  </style>
+    <link rel="stylesheet" href="./css/index.css" ></link>
 </head>
 <body>
     <div class="container">
